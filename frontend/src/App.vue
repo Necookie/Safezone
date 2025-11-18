eeca8cfb2d8f122282a7d0a21d22feeb1a06d814
DheynMichaelOrlanda <dheyn.alt@gmail.com>
2025-11-19 02:25:22 +0800

    Initial commit: backend + frontend + mobile setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <GameFrame />
+  </div>
+</template>
+
+<script>
+import GameFrame from './components/GameFrame.vue'
+
+export default {
+  components: {
+    GameFrame
+  }
+}
+</script>
